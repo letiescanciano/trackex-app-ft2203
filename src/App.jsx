4aa56492ea2de29d79103db8077ce5ae3ff93340
letiescanciano <leticia.escanciano@gmail.com>
2022-06-07 16:52:52 +0200

    M7_S2

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,76 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Flex,
-} from '@chakra-ui/react';
+
 
 import { Logo } from './Logo';
+import './App.css';
+
+import styled from 'styled-components';
+
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const SideNavBar = styled.div`
+  width: 250px;
+  padding: 16px;
+`;
+
+const LogoTitle = styled.h1`
+  margin-left: 8px;
+  color: #ff7661;
+`;
+
+const List = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+//     color: ${props => {
+//   console.log('props', props);
+//   if (props.isActive === true) {
+//     return '#FF7661';
+//   } else {
+//     return 'white';
+//   }
+// }};
+const ListItem = styled.li`
+  padding: 16px 32px;
+  a {
+    color: ${props => (props.isActive ? '#FF7661' : 'white')};
+    font-weight: ${props => (props.isActive ? '500' : '200')};
+    text-decoration: none;
+  }
+`;
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box display="flex" backgroundColor="blue.900" height="100vh">
+    <div className="layout">
+      <SideNavBar>
+        <LogoWrapper>
+          <Logo></Logo>
+          <LogoTitle>TrackEx</LogoTitle>
+        </LogoWrapper>
         <nav>
-          <Flex>
-            <Logo />
-            <Text color="orange.500" fontWeight="bold" fontSize="3xl">
-              Trackex
-            </Text>
-          </Flex>
-          <ul>
-            <li>Dashboard</li>
-            <li>Calendar</li>
-            <li>Transactions</li>
-          </ul>
+          <List>
+            <ListItem>
+              <a href="/">Dashboard</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Calendar</a>
+            </ListItem>
+            <ListItem isActive={true}>
+              <a href="/">Transactions</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Settings</a>
+            </ListItem>
+          </List>
         </nav>
-        <main> Main</main>
-      </Box>
-    </ChakraProvider>
+      </SideNavBar>
+      <main></main>
+    </div>
   );
 }
 
